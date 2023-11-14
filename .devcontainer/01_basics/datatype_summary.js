951c3f93bf7da3c30925bc19ad8312dfb378fcfe
aadil ansari <142161929+Aadilan@users.noreply.github.com>
2023-11-14 01:54:41 +0000

    datatype_summary.js

---
+++ .devcontainer/01_basics/datatype_summary.js
@@ -0,0 +1,21 @@
+//primitive
+
+
+//7 types: string,number,null,undifined,boolean,bigInt,symbol
+
+
+
+//referance(non primative)
+
+//array,object,functions
+
+const heros =["shaktiman","naagraj","doga"];
+let myObj ={
+    name:"aadil",
+    age:22,
+}
+
+ const myFunction = function(){
+    console.log("hellowould")
+ }
+ console.log(typeof myFunction);
