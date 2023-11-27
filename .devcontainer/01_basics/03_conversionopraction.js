19963e017117947852a91bd273839a7e15ba796d
aadil ansari <142161929+Aadilan@users.noreply.github.com>
2023-11-27 10:19:06 +0000

    container config done

---
+++ .devcontainer/01_basics/03_conversionopraction.js
@@ -0,0 +1,51 @@
+let score ="33"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+
+
+//onversion output
+// "33" = 33
+//"33abc" = NaN 
+// True = 1
+//false = 0 ;
+
+let isLoggedIn = ""
+ let booleanisLoggedIn = Boolean(isLoggedIn)
+ //console.log(booleanisLoggedIn)
+
+ //1 = true
+ //0 = false 
+ //"" = false 
+ //"aadil" = true
+   let someNumber = 33
+
+   let stringNumber = String(someNumber)
+   //console.log(typeof stringNumber);
+
+
+
+
+  // **********************opractions****************
+ 
+  //console.log(2 +2);
+  //console.log(2-2);
+  //console.log(2 *2);
+  //console.log(2 %2);
+  //console.log(2**3);
+
+
+  let str1 = "aadil"
+  let str2 = " ansari"
+
+  let str3 = str1+ str2;
+  //console.log(str3)
+
+  //console.log("1" +2)
+  //console.log(1 +"4" +3)
+  //console.log(1 + 3 + "5")
+  //console.log(+true);
+  //console.log(+"");
