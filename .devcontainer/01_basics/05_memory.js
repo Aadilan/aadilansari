759d9690620693f7b9b6d85d6b033cc5cdd3579c
aadil ansari <142161929+Aadilan@users.noreply.github.com>
2023-11-27 15:16:04 +0000

    stack&heap

---
+++ .devcontainer/01_basics/05_memory.js
@@ -0,0 +1,16 @@
+// stack(primative use)  , heap(non-primative)
+
+let myYoutabename = "aadilansari9450@gmail.com"
+let anotherName = myYoutabename
+anotherName = "chai aur code"
+//console.log(myYoutabename);
+//console.log(anotherName);
+
+let userone = {
+    email:"rashadansari988@gmail.com",
+    upi :"user@ybl"
+}
+let usertwo= userone
+usertwo.email = "aadilansari9436@gmail.com"
+console.log(userone.email);
+console.log(usertwo.email);
