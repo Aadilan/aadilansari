35d66f9370ddf63dc9288958ee6b81bce30061b9
aadil ansari <142161929+Aadilan@users.noreply.github.com>
2023-11-27 12:38:24 +0000

    comparision

---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+//console.log(3>5);
+//console.log(4<5);
+//console.log(4==5);
+
+//console.log("2" >1);
+//console.log("02" > 1); not answers is predicatable dont use // because datatype is not same.
+
+
+//console.log(null >0);
+//console.log(null == 0);
+//console.log(null>=0);
+
+//console.log(undefined ==0);
+//console.log(undefined > 0);
+//console.log(undefined < 0);
+
+
+// ===
+console.log("2" === 2);
+
