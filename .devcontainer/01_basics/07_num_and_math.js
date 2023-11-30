d356c90c63dbf24c23ed4733446d7e06ef783f2c
aadil ansari <142161929+Aadilan@users.noreply.github.com>
2023-11-30 01:59:26 +0000

    math_num

---
+++ .devcontainer/01_basics/07_num_and_math.js
@@ -0,0 +1,35 @@
+const score = 400
+//console.log(score);
+
+const balance =  new Number  (100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(typeof balance);
+//console.log(balance.toFixed(3));
+
+const otherNumber = 123.
+
+//console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000000
+//console.log(hundreds.toLocaleString('en-in'))
+
+
+//**********************MATH*********************** */
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9));
+// console.log(Math.min(2,4,566,7,8,0,))
+// console.log(Math.max(2,7,9,0,5,66,))
+
+console.log(Math.random());
+console.log(Math.random()*10 +1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min +1))+ min);
+
