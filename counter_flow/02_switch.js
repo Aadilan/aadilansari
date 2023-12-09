838e190038bf14e8a5f1f797194e9bee52d28c05
aadil ansari <142161929+Aadilan@users.noreply.github.com>
2023-12-09 10:58:41 +0000

    switch

---
+++ counter_flow/02_switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+0
+const month = 3
+ switch (month) {
+    
+    case 1:
+        console.log("january")
+        
+        break;
+        case 2:
+        console.log("faburary")
+        
+        break;
+        case 3:
+        console.log("march")
+        
+        break;
+ 
+    default:
+        console.log("nothing value")
+        break;
+ }
